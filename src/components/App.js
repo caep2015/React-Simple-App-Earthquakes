303ca7a69c17c19a636d9466f5a9f22ab8af516b
Carlota Pearl <carlota.pearl@gmail.com>
2017-08-23 21:20:13 -0400

    List of 8 Earth Quakes - Hard Mode next...

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import EarthquakeList from './EarthquakeList'
+import EarthquakeInfo from './EarthquakeInfo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="title">
-          <div className="my-header">Earthquakes!</div>
+          <div className="my-header">
+            <h3>Earthquakes!</h3>
+          </div>
+          <EarthquakeInfo />
+          <EarthquakeList />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
